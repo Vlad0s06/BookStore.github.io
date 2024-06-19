5ce94d88070cca242bef08f7f603c0e2aff93bb0
Vlad0s06 <96639700+Vlad0s06@users.noreply.github.com>
2024-06-19 11:50:46 +0700

    Add files via upload

---
+++ dbo.DeliveryDetails.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[DeliveryInfo] (
+    [DeliveryInfoId] INT            IDENTITY (1, 1) NOT NULL,
+    [FullName]          NVARCHAR (MAX) NOT NULL,
+    [Postcode]          NVARCHAR (MAX) NOT NULL,
+    [Country]           NVARCHAR (MAX) NOT NULL,
+    [City]              NVARCHAR (MAX) NOT NULL,
+    [Street]            NVARCHAR (MAX) NOT NULL,
+    [Building]          NVARCHAR (MAX) NOT NULL,
+    [Appartment]      NVARCHAR (MAX) NOT NULL,
+    CONSTRAINT [PK_dbo.DeliveryInfo] PRIMARY KEY CLUSTERED ([DeliveryInfoId] ASC)
+);
+
---
+++ dbo.Orders.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Orders] (
+    [OrderId]       INT           IDENTITY (1, 1) NOT NULL,
+    [BookId]            INT           NOT NULL,
+    [Quantity]          INT           NOT NULL,
+    [DeliveryDetailsId] INT           NOT NULL,
+    [UserId]            INT           NOT NULL,
+    [DeliveryStatus]    NVARCHAR (25) NOT NULL,
+    CONSTRAINT [PK_dbo.Orders] PRIMARY KEY CLUSTERED ([OrderId] ASC)
+);
+
