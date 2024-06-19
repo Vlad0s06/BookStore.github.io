a42ec4e8660da184591047593c01714e5c0a89ae
Vlad0s06 <96639700+Vlad0s06@users.noreply.github.com>
2024-06-19 11:19:23 +0700

    Delete BookStore.Domain directory

--- BookStore.Domain/Abstract/IOrderProcessor.cs
+++
@@ -1,9 +0,0 @@
-﻿using BookStore.Domain.Entities;
-
-namespace BookStore.Domain.Abstract
-{
-    public interface IOrderProcessor
-    {
-        void ProcessOrder(Cart cart, DeliveryInfo deliveryInfo);
-    }
-}
