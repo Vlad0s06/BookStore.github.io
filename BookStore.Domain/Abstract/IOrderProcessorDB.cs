cf28f77ef5ec0fe7d4b8c9a00c481fef36c29951
Vlad0s06 <96639700+Vlad0s06@users.noreply.github.com>
2024-06-19 11:09:33 +0700

    Add files via upload

---
+++ BookStore.Domain/Abstract/IOrderProcessorDB.cs
@@ -0,0 +1,13 @@
+﻿using BookStore.Domain.Entities;
+using System.Collections.Generic;
+
+namespace BookStore.Domain.Abstract
+{
+    public interface IOrderProcessorDb
+    {
+        IEnumerable<Purchase> Purchases { get; }
+        void ProcessOrderDB(Cart cart, DeliveryInfo deliveryInfo, User user);
+        void ChangeDeliveryStatus(int orderId, string status);
+        void DeletePurchase(int orderId);
+    }
+}
